3a648ecce46c10fefc122eb2b99597c03bf9927f
Jeremy Buttice <jeremy@tymate.com>
2017-07-15 20:26:19 +0200

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+
+Bundler.require(*Rails.groups)
+
+module Scratchagift
+  class Application < Rails::Application
+    config.load_defaults 5.1
+    config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any,
+                      methods: %i[get post options put head delete]
+      end
+    end
+  end
+end
